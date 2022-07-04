08e30facc4e9592b1a45af01bb1784366ff18203
Katie Walters <katie_walters@live.co.uk>
2022-07-04 11:04:57 +0100

    package json set up and index.js

---
+++ Desktop/SoC_Projects/SoC_Week10/Week10_WeekendPractice/my-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import LoginButton from './Components/LoginButton';
+import LogoutButton from './Components/LogoutButton';
+import Profile from './Components/Profile';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
